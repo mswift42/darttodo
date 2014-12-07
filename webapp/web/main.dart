aa07e3fb1e2995024f37960928aabe1c75654b95
martin.haesler <martin.haesler@gmail.com>
2014-12-07 16:38:52 +0000

    add route for /newtask

--- webapp/web/main.dart
+++ webapp/web/main.dart
@@ -4,18 +4,20 @@
 import 'dart:html';
 
 import 'package:webapp/nav_menu.dart';
-import 'package:webapp/reverser.dart';
+//import 'package:webapp/reverser.dart';
 import 'package:route_hierarchical/client.dart';
 
 void main() {
   initNavMenu();
-  initReverser();
+//  initReverser();
 
   // Webapps need routing to listen for changes to the URL.
   var router = new Router();
   router.root
     ..addRoute(name: 'about', path: '/about', enter: showAbout)
-    ..addRoute(name: 'home', defaultRoute: true, path: '/', enter: showHome);
+    ..addRoute(name: 'home', defaultRoute: true, path: '/', enter: showHome)
+    ..addRoute(name: 'newtask', path: '/newtask', enter: showNewTask);
+
   router.listen();
 }
 
@@ -23,9 +25,17 @@ void showAbout(RouteEvent e) {
   // Extremely simple and non-scalable way to show different views.
   querySelector('#home').style.display = 'none';
   querySelector('#about').style.display = '';
+  querySelector('#newtask').style.display = 'none';
 }
 
 void showHome(RouteEvent e) {
   querySelector('#home').style.display = '';
   querySelector('#about').style.display = 'none';
+	querySelector('#newtask').style.display='none';
+}
+
+void showNewTask(RouteEvent e) {
+	querySelector('#home').style.display='none';
+	querySelector('#about').style.display='none';
+	querySelector('#newtask').style.display='active';
 }
